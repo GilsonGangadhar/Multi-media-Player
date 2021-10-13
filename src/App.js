1b46d293d49556e662b679295ab6177c38b4e039
GilsonGangadhar <gilsongangadhar@gmail.com>
2021-10-13 13:53:32 +0530

    creating sidebar, videopage and loginpage & styling it

--- src/App.js
+++ src/App.js
@@ -1,23 +1,32 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Container } from 'react-bootstrap'
 import Header from "./components/header/Header"
 import Sidebar from './components/sidebar/Sidebar'
 import HomeScreen from './screens/HomeScreen'
+import LoginScreen from './screens/loginScreen/LoginScreen'
 
 import "./_app.scss"
 
 const App = () => {
-  return (
-    <>
-      <Header />
 
-      <div className="app__container border border-info">
-        <Sidebar/>
-        <Container fluid className="app__main border border-warning">
-          <HomeScreen/>
-        </Container>
-      </div>
-    </>
+  const [sidebar, toggleSidebar] = useState(false)
+
+  const handleToogleSidebar = () => {
+    toggleSidebar(value => !value)
+  }
+  return (
+    // <>
+    //   <Header handleToogleSidebar={handleToogleSidebar} />
+
+    //   <div className="app__container border border-info">
+    //     <Sidebar sidebar={sidebar} handleToogleSidebar={handleToogleSidebar}/>
+    //     <Container fluid className="app__main border border-warning">
+    //       <HomeScreen/>
+    //     </Container>
+    //   </div>
+    // </>
+
+    <LoginScreen />
   )
 }
 
--- src/components/video/Video.js
+++ src/components/video/Video.js
@@ -1,10 +1,26 @@
 import React from 'react'
+import {AiFillEye} from 'react-icons/ai'
 import "./_video.scss"
 
 const Video = () => {
     return (
-        <div>
-            video
+        <div className="video">
+            <div className="video__top">
+                <img src="https://i.ytimg.com/vi/ftJKqKuppF4/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLBTyB2p-1QT18NxSiLP7G2U-qUk_w" alt=""/>
+                <span>05.43</span>
+            </div>
+            <div className="video__title">
+                Create app in 5 minutes #made by chintu
+            </div>
+            <div className="video__details">
+                <span>
+                <AiFillEye /> 5m Views •
+                </span>
+            </div>
+            <div className="video__channel">
+                <img src="https://yt3.ggpht.com/ytc/AKedOLS2hTRYZWpqcVUPi1GEW6M4GZgAfPSOcXy9c0U-pA=s68-c-k-c0x00ffffff-no-rj" alt=""/>
+                <p>Rainbow Hat jr</p>
+            </div>
         </div>
     )
 }
