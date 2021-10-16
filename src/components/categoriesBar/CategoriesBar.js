4a34f58401a8816b7178f262532d587ee4b1b217
GilsonGangadhar <gilsongangadhar@gmail.com>
2021-10-16 16:10:16 +0530

    debugging4

--- src/components/categoriesBar/CategoriesBar.js
+++ src/components/categoriesBar/CategoriesBar.js
@@ -1,58 +1,56 @@
 import React, { useState } from 'react'
 import { useDispatch } from 'react-redux'
-import { getPopularVideos, getVideosByCategory } from '../../redux/actions/videos.action'
-import "./_categoriesBar.scss"
+import {
+   getPopularVideos,
+   getVideosByCategory,
+} from '../../redux/actions/videos.action'
+import './_categoriesBar.scss'
 
 const keywords = [
-    'All',
-    'React js',
-    'Next js',
-    'React Native',
-    'use of API',
-    'Redux',
-    'Music',
-    'Algorithm Art ',
-    'Guitar',
-    'Hindi Songs',
-    'Coding',
-    'Cricket',
-    'Football',
-    'Barcelona',
-    'Gatsby',
-    'Story Recapped',
-    'Ghost Series',
- ]
+   'All',
+   'React js',
+   'Angular js',
+   'React Native',
+   'use of API',
+   'Redux',
+   'Music',
+   'Algorithm Art ',
+   'Guitar',
+   'Bengali Songs',
+   'Coding',
+   'Cricket',
+   'Football',
+   'Real Madrid',
+   'Gatsby',
+   'Poor Coder',
+   'Shwetabh',
+]
 
 const CategoriesBar = () => {
+   const [activeElement, setActiveElement] = useState('All')
 
-    const [activeElement, setActiveElement] = useState('All')
+   const dispatch = useDispatch()
+   const handleClick = value => {
+      setActiveElement(value)
+      if (value === 'All') {
+         dispatch(getPopularVideos())
+      } else {
+         dispatch(getVideosByCategory(value))
+      }
+   }
 
-    const dispatch = useDispatch()
-
-    const handleClick = (value) => {
-        setActiveElement(value)
-        if (value === 'All') {
-            dispatch(getPopularVideos())
-         } else {
-            dispatch(getVideosByCategory(value))
-         }
-    }
-
-    console.log(activeElement, "activeElement")
-    return (
-        <div className="categoriesBar">
-            {
-                keywords.map((value,i) => (
-                    <span 
-                    onClick={()=> handleClick(value)}
-                    key={i}
-                    className={activeElement === value? "active" : ""}
-                    >{value}</span>
-                   
-                ))
-            }
-        </div>
-    )
+   return (
+      <div className='categoriesBar'>
+         {keywords.map((value, i) => (
+            <span
+               onClick={() => handleClick(value)}
+               key={i}
+               className={activeElement === value ? 'active' : ''}>
+               {value}
+            </span>
+         ))}
+      </div>
+   )
 }
 
 export default CategoriesBar
